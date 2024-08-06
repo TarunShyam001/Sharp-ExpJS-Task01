355e43830f0029ac552ad157220042f8d5f27373
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-06 19:55:10 +0530

    res.send() functionality is performed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use((req, res, next)=>{
     console.log('middleware is get called');
+    res.send(`<h1> Welcome to Express.JS</h1>`);
     next();
 });
 app.use((req, res, next)=>{
