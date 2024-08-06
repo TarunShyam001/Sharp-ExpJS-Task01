4aae0127407e27f05725eb9d2d937ccfd87133e4
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-06 19:49:33 +0530

    Middleware task is performed

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next)=>{
+    console.log('middleware is get called');
+});
+app.use((req, res, next)=>{
+    console.log('another middleware is get called');
+});
 const PORT = 1300;
 
 const server = http.createServer(app);
