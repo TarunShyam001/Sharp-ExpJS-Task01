2ce094b9e43bffccfa2c4542d360611e93c833bd
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-06 21:49:38 +0530

    body-parser is installed

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('middleware is get called');
-    next();
+app.use('/add-product',(req, res, next)=>{
+    res.send(`<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product </button><br><input type="number" min="0"><button type="submit"> Add Size </button></form></body></html>`)
 });
-app.use((req, res, next)=>{
-    console.log('another middleware is get called');
-    res.send(`<h1> Welcome to Express.JS</h1>`);
+
+app.use('/product', (req, res, next)=>{
+    console.log(res.body);
+    res.redirect('/');
 });
+
+app.use('/', (req, res)=>{
+    console.log('Task performed');
+    res.send('<h1> Hello to the ExpressJS </h1>');
+})
+
 const PORT = 1300;
 
 app.listen(PORT, () => {
