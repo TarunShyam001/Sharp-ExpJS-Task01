1381d73e168549375c4254fcad38f249c7131369
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-31 22:04:41 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/add-product',(req, res, next)=>{
     res.send(`<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="number" min="0" name="size" ><button type="submit"> Add Product </button></form></body></html>`);
 });
 
-app.use('/product', (req, res, next)=>{
+app.post('/product', (req, res, next)=>{
     console.log(req.body.title);
     console.log(req.body.size);
     res.redirect('/');
